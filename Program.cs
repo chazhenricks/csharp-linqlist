cb560640712214d33d82efcc0d0b65a0cc48a550
Chaz Henricks <chazhenricks@gmail.com>
2017-07-11 09:47:13 -0500

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace linqlist
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
